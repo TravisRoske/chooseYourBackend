8e9a33c2fbf4a28cf8adbc297f8cd8f1d24faa5d
Travis Roske <53085855+TravisRoske@users.noreply.github.com>
2023-04-07 18:14:55 -0600

    Delete Backend/DBServer/Build directory

--- Backend/DBServer/Build/manager/dbManagerRouter.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// import express from 'express'
-// import { update } from './dbManager.js'
-// export const dbManagerRouter = express.Router();
-// dbManagerRouter.route()
-//     .all(update)
