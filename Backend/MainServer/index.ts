d86ef09186d1956413cc9cc754be46d902f35f0c
stableGeniuses <TravisRoske@gmail.com>
2023-04-09 15:41:53 -0700

    .

--- Backend/MainServer/index.ts
+++ Backend/MainServer/index.ts
@@ -18,7 +18,7 @@ const app = express();
 
 const limiter = rateLimit({
 	windowMs: 10 * 1000, // 10 seconds
-	max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
