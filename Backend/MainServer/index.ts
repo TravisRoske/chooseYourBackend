4d4e35619b93ad0eba6dcdc2dd2be5da90763522
stableGeniuses <TravisRoske@gmail.com>
2023-04-09 15:35:04 -0700

    .

--- Backend/MainServer/index.ts
+++ Backend/MainServer/index.ts
@@ -1,14 +1,16 @@
-import express from 'express'
-import axios from 'axios'
+import express from 'express';
+import axios from 'axios';
 
-import path from 'path'
+import path from 'path';
 
-const cors = require('cors')//////////////
-//add helmet///////
-//add a rate limiter/////
+const cors = require('cors');//////////////
+import rateLimit from 'express-rate-limit';
 
-import { postgresForwarder } from './postgres/forwardPostgres.js'
-import { mysqlForwarder } from './mysql/forwardMysql.js'
+import helmet from 'helmet';
+
+
+import { postgresForwarder } from './postgres/forwardPostgres.js';
+import { mysqlForwarder } from './mysql/forwardMysql.js';
 
 const port = process.env.port;
 const dbMasterUrl = process.env.dbMasterUrl;
@@ -16,13 +18,35 @@ const dbMasterUrl = process.env.dbMasterUrl;
 const app = express();
 
 
-//I'll have to change this later because this just allows everything//////
+const limiter = rateLimit({
+	windowMs: 10 * 1000, // 10 seconds
+	max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+const limiter2 = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 2000, // Limit each IP to 2000 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
+app.use(limiter2)
+
+
+app.use(helmet());
+
+//I'll have to change this later because this just allows everything/////////
 app.use(cors());
 
 app.use(express.json());
 
 
 //app.get("/assignid" .....)
+////////make this a forwarder function
 app.get("/assignid/:userid", (req, res) => {
     const options = {
         url: dbMasterUrl + '/assignid/' + req.params.userid,
