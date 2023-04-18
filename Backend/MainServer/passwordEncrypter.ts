c9d2be7fc401188fe5f2dba9f8c0d31aa9e2cdd5
stableGeniuses <TravisRoske@gmail.com>
2023-04-17 23:08:59 -0700

    .

--- Backend/MainServer/passwordEncrypter.ts
+++ Backend/MainServer/passwordEncrypter.ts
@@ -1,23 +1,31 @@
-// import crypto from 'crypto'
-// import bcrypt from 'bcrypt'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
 
 
-//middleware function
-    //calls encrypt
+async function forward(req: any, res: any, next: any) {
+    if(req?.body?.password){
+        req.body.password = encrypt(req.body.password, req.query.encryption)
+    }
+}
 
 
-// function encrypt( password : String, encryption : String ) : String {
+function encrypt( password : string, encryption : string ) : string {
 
-//     switch(encryption) {
-//         case "none" :
-//             return password;
-//             break;
-//         case "sha256" :
+    switch(encryption) {
+        case "none" :
+            return password;
+            break;
+        case "sha256" :
 
-//             break;
-//         case "bcrypt" :
+            break;
+        case "bcrypt" :
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(password, salt, function(err, hash) {
+                    return hash;
+                });
+            })
+            break;
+    }
+    return ""
 
-//             break;
-//     }
-
-// }
+}
